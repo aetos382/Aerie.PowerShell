29e0cd5fea26dcaad9e9f66894448c176d1ff7f6
aetos <aetos@aerie.jp>
2018-11-06 01:33:07 +0900

    これで…いいのか？

--- src/analyzers/Aerie.PowerShell.Async.Analyzer/DisposeDiagnosticAnalyzer.cs
+++ src/analyzers/Aerie.PowerShell.Async.Analyzer/DisposeDiagnosticAnalyzer.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Management.Automation;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace Aerie.PowerShell.Async.Analyzer
@@ -33,29 +30,113 @@ namespace Aerie.PowerShell.Async.Analyzer
             CompilationStartAnalysisContext context)
         {
             var cmdletSymbol = context.Compilation.GetTypeByMetadataName(typeof(Cmdlet).FullName);
-            var disposableSymbol = context.Compilation.GetTypeByMetadataName(typeof(IDisposable).FullName);
             var asyncCmdletSymbol = context.Compilation.GetTypeByMetadataName(typeof(IAsyncCmdlet).FullName);
 
+            var doDisposeSymbol = (IMethodSymbol)context.Compilation
+                                         .GetTypeByMetadataName(typeof(AsyncCmdletExtensions).FullName)
+                                         .GetMembers(nameof(AsyncCmdletExtensions.DoDispose))[0];
+
             context.RegisterSyntaxNodeAction(
-                syntaxContext => AnalyzeMethod(syntaxContext, cmdletSymbol, disposableSymbol, asyncCmdletSymbol),
-                SyntaxKind.MethodDeclaration);
+                syntaxContext => AnalyzeClass(
+                    syntaxContext,
+                    cmdletSymbol,
+                    asyncCmdletSymbol,
+                    doDisposeSymbol),
+                SyntaxKind.ClassDeclaration);
         }
 
-        private static void AnalyzeMethod(
+        private static void AnalyzeClass(
             SyntaxNodeAnalysisContext context,
             INamedTypeSymbol cmdletType,
-            INamedTypeSymbol disposableType,
-            INamedTypeSymbol asyncCmdletType)
+            INamedTypeSymbol asyncCmdletType,
+            IMethodSymbol doDisposeMethodSymbol)
         {
-            if (!(context.ContainingSymbol is IMethodSymbol methodSymbol))
+            if (!(context.ContainingSymbol is INamedTypeSymbol namedTypeSymbol))
             {
                 return;
             }
 
-            if (!IsAsyncCmdlet(methodSymbol.ContainingType, cmdletType, asyncCmdletType))
+            if (!IsAsyncCmdlet(namedTypeSymbol, cmdletType, asyncCmdletType))
             {
                 return;
             }
+
+            var disposableType = context.Compilation.GetSpecialType(SpecialType.System_IDisposable);
+            var disposeMethod = disposableType.GetMembers(nameof(IDisposable.Dispose))[0];
+            var disposeMethodImplementation = (IMethodSymbol)namedTypeSymbol.FindImplementationForInterfaceMember(disposeMethod);
+
+            bool foundDoDispose = DoDisposeCalled(context, disposeMethodImplementation, doDisposeMethodSymbol);
+
+            if (!foundDoDispose)
+            {
+                var diagnostic = Diagnostic.Create(
+                    ShouldCallDoDisposeDescriptor.Rule,
+                    disposeMethodImplementation.Locations[0]);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private static bool DoDisposeCalled(
+            SyntaxNodeAnalysisContext context,
+            IMethodSymbol symbol,
+            IMethodSymbol definitionSymbol,
+            bool innerScope = false)
+        {
+            var location = symbol.Locations[0];
+
+            if (!location.IsInSource)
+            {
+                return false;
+            }
+
+            var node = context.Node.FindNode(location.SourceSpan);
+            var invocations = node.DescendantNodes().Where(n => n.IsKind(SyntaxKind.InvocationExpression));
+
+            foreach (var invocation in invocations)
+            {
+                var symbol2 = (IMethodSymbol)context.SemanticModel.GetSymbolInfo(invocation).Symbol;
+
+                if (IsDoDisposeCall(symbol2, definitionSymbol))
+                {
+                    return true;
+                }
+
+                if (!innerScope &&
+                    symbol2.Name == "Dispose" &&
+                    symbol2.Parameters.Length == 1 &&
+                    symbol2.Parameters[0].Type.SpecialType == SpecialType.System_Boolean)
+                {
+                    if (DoDisposeCalled(context, symbol2, definitionSymbol, true))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private static bool IsDoDisposeCall(
+            IMethodSymbol symbol,
+            IMethodSymbol definitionSymbol)
+        {
+            if (symbol.MethodKind == MethodKind.ReducedExtension)
+            {
+                symbol = symbol.GetConstructedReducedFrom();
+
+                if (symbol.IsGenericMethod)
+                {
+                    symbol = symbol.OriginalDefinition;
+
+                    if (symbol.Equals(definitionSymbol))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
         }
 
         private static bool IsAsyncCmdlet(
