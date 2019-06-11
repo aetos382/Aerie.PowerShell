3d799a93b5bf0f3e84e03a477475b41bc2d78896
aetos <aetos@aerie.jp>
2019-06-12 01:17:39 +0900

    add DynamicParameterObjectProviderAttribute

---
+++ src/Aerie.PowerShell.DynamicParameter/DynamicParameterObjectProviderAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using JetBrains.Annotations;
+
+namespace Aerie.PowerShell
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class DynamicParameterObjectProviderAttribute :
+        Attribute
+    {
+        public DynamicParameterObjectProviderAttribute(
+            [NotNull] Type providerType)
+        {
+            if (providerType is null)
+            {
+                throw new ArgumentNullException(nameof(providerType));
+            }
+
+            if (!ValidateProviderType(providerType))
+            {
+                throw new ArgumentException();
+            }
+
+            this.ProviderType = providerType;
+        }
+
+        [NotNull]
+        public Type ProviderType { get; }
+
+        private static bool ValidateProviderType(
+            [NotNull] Type providerType)
+        {
+            if (!typeof(IDynamicParameterObjectProvider).IsAssignableFrom(providerType))
+            {
+                return false;
+            }
+
+            if (!providerType.IsDefaultConstructible())
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Aerie.PowerShell.DynamicParameter/TypeExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using JetBrains.Annotations;
+
+namespace Aerie.PowerShell
+{
+    public static class TypeExtensions
+    {
+        public static bool IsDefaultConstructible(
+            [NotNull] this Type type)
+        {
+            if (type is null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            if (type.IsGenericType)
+            {
+                if (!type.IsConstructedGenericType)
+                {
+                    return false;
+                }
+            }
+
+            if (type.IsInterface)
+            {
+                return false;
+            }
+
+            if (type.IsAbstract)
+            {
+                return false;
+            }
+
+            var constructor = type.GetConstructor(Type.EmptyTypes);
+
+            if (constructor is null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
