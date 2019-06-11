9639ad020182f8faed1238ab71577ecfe70c654b
aetos <aetos@aerie.jp>
2019-06-11 22:44:55 +0900

    add DynamicParameterAttributeProvider

---
+++ src/Aerie.PowerShell.DynamicParameter/DefaultDynamicParameterAttributeProvider.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using JetBrains.Annotations;
+
+namespace Aerie.PowerShell
+{
+    public class DefaultDynamicParameterAttributeProvider :
+        IDynamicParameterAttributeProvider
+    {
+        [NotNull]
+        private static readonly Dictionary<MemberInfo, List<CustomAttributeData>> _cache =
+            new Dictionary<MemberInfo, List<CustomAttributeData>>();
+
+        public IEnumerable<CustomAttributeData> GetAttributeData(
+            MemberInfo member)
+        {
+            if (member is null)
+            {
+                throw new ArgumentNullException(nameof(member));
+            }
+
+            if (!_cache.TryGetValue(member, out var attributes))
+            {
+                _cache[member] = attributes = new List<CustomAttributeData>();
+
+                foreach (var attribute in member.CustomAttributes)
+                {
+                    var attributeType = attribute.AttributeType;
+
+                    if (attributeType == typeof(DynamicParameterAttribute))
+                    {
+                        attributes.Add(new ParameterAttributeData(attribute));
+                    }
+                    else if (!Attribute.IsDefined(attributeType, typeof(DynamicParameterInternalAttribute)))
+                    {
+                        attributes.Add(attribute);
+                    }
+                }
+            }
+
+            return attributes;
+        }
+
+        [NotNull]
+        public static readonly DefaultDynamicParameterAttributeProvider Instance =
+            new DefaultDynamicParameterAttributeProvider();
+    }
+}
---
+++ src/Aerie.PowerShell.DynamicParameter/IDynamicParameterAttributeProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using JetBrains.Annotations;
+
+namespace Aerie.PowerShell
+{
+    public interface IDynamicParameterAttributeProvider
+    {
+        [NotNull]
+        [ItemNotNull]
+        IEnumerable<CustomAttributeData> GetAttributeData(
+            [NotNull] MemberInfo member);
+    }
+}
